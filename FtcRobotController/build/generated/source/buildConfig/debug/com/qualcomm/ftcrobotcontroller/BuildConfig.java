6f7b311baebf3b564153b79177ab62fc6cdff138
jevgome <jevgome7@gmail.com>
2024-02-09 07:54:31 -0500

    init

---
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qualcomm.ftcrobotcontroller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String APP_BUILD_TIME = "2024-02-08T20:15:47.043-0500";
+}
